642029ea64355cd89da2a408ae7b83997bb4c5a3
SChervonyi <bekyts@gmail.com>
2016-02-20 21:50:31 +0200

    Add Domain Lib

---
+++ CarSellApp/Domain/EFDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	class EFDbContext : DbContext
+	{
+		public EFDbContext(string connectionString)
+		{
+			Database.Connection.ConnectionString = connectionString;
+		}
+	}
+}
